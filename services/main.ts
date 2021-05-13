50c47e501f4709d21cf7910978cbadc7a7686d4e
Liam-Gardner <liam@flipdish.ie>
2021-05-13 15:53:07 +0100

    hardcode cluster

--- services/main.ts
+++ services/main.ts
@@ -9,10 +9,11 @@ export const callSageMaker = async (data: {
       "https://4dzakqq057.execute-api.eu-west-1.amazonaws.com/dev/predictprofile",
       { data: data.data }
     );
-    if (response.data.length) {
-      const num = response.data[0].closest_cluster.toString().split(".")[0];
-      return num;
-    } else return [] as any
+    return "1"
+    // if (response.data.length) {
+    //   const num = response.data[0].closest_cluster.toString().split(".")[0];
+    //   return num;
+    // } else return [] as any
   } catch (error) {
     console.log("sage call error", error);
   }
